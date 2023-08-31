f48efbd521a6952443c601640a74a0da8baeea85
Sikyin Tang <thomas2d19@gmail.com>
2023-08-31 03:00:41 -0400

    Change anchor in Markdown preview to open link in new tab

--- src/components/MarkdownOutput.tsx
+++ src/components/MarkdownOutput.tsx
@@ -1,17 +1,30 @@
 import { FC } from "react";
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { Components } from "react-markdown";
+import "./MarkdownOutput.css";
 
 interface MarkdownOutputProps {
   markdown: string;
 }
 
+const markdownComponents: Components = {
+  a: ({ children, ...props }) => (
+    <a {...props} target="_blank">
+      {children}
+    </a>
+  ),
+};
 const MarkdownOutput: FC<MarkdownOutputProps> = ({ markdown }) => {
   return (
     <div className="h-full flex flex-col overflow-hidden bg-zinc-800">
       <div className="h-full overflow-auto p-2">
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+        <ReactMarkdown
+          className="react-markdown-components"
+          components={markdownComponents}
+        >
+          {markdown}
+        </ReactMarkdown>
       </div>
-      <hr className="border-2 border-zinc-900" />{" "}
+      <hr className="border-2 border-zinc-900" />
       <p className="p-2 text-base text-center">Preview</p>
     </div>
   );
