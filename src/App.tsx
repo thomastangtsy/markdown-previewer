66bafe0f5d873fa3f5c0875621bc0c4382cdd885
Sikyin Tang <thomas2d19@gmail.com>
2023-08-31 00:41:59 -0400

    Add header to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <></>;
+  return (
+    <div className="w-screen h-[200vh] md:h-screen">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
