9c4dc244b6ad97a54c5b2de7bea34547cd395842
Sikyin Tang <thomas2d19@gmail.com>
2023-09-01 09:50:45 -0400

    Change the guide close button text to icon

--- src/components/MarkdownGuide.tsx
+++ src/components/MarkdownGuide.tsx
@@ -11,35 +11,35 @@ const markdownSyntaxExamples: Record<string, string> = {
 
 const MarkdownGuide: FC = () => {
   return (
-      <table className="w-full">
-        {Object.keys(markdownSyntaxExamples).map((type) => {
-          return (
-            <Fragment key={type}>
-              <tr className="mb-4">
-                <th colSpan={3}>
-                  <h4 className="text-2xl font-bold">{type}</h4>
-                  <hr className="border-zinc-600 mb-2" />
-                </th>
-              </tr>
-              <tr>
-                <td className="w-[48%]">
-                  <code className="whitespace-pre-line min-w-min break-words">
-                    {markdownSyntaxExamples[type]}
-                  </code>
-                </td>
-                <td className="w-[4%] mx-4">
-                  <p>➡️</p>
-                </td>
-                <td className="w-[48%]">
-                  <Markdown className="min-w-min">
-                    {markdownSyntaxExamples[type]}
-                  </Markdown>
-                </td>
-              </tr>
-            </Fragment>
-          );
-        })}
-      </table>
+    <table className="w-full">
+      {Object.keys(markdownSyntaxExamples).map((type) => {
+        return (
+          <Fragment key={type}>
+            <tr className="mb-4">
+              <th colSpan={3}>
+                <h4 className="text-2xl font-bold">{type}</h4>
+                <hr className="border-zinc-600 mb-2" />
+              </th>
+            </tr>
+            <tr>
+              <td className="w-[48%]">
+                <code className="whitespace-pre-line min-w-min break-words">
+                  {markdownSyntaxExamples[type]}
+                </code>
+              </td>
+              <td className="w-[4%] mx-4">
+                <p>➡️</p>
+              </td>
+              <td className="w-[48%]">
+                <Markdown className="min-w-min">
+                  {markdownSyntaxExamples[type]}
+                </Markdown>
+              </td>
+            </tr>
+          </Fragment>
+        );
+      })}
+    </table>
   );
 };
 
